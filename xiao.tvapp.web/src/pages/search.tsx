5a65c8ce27279dff481a5b5928081c7171c3a7e3
hirotaka42 <kodama.hirotaka.190380@roivy.net>
2024-06-17 23:10:15 +0900

    feat: アイテム検索ページのベース作成

---
+++ xiao.tvapp.web/src/pages/search.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next';
+import { SearchBar } from '../components/Atoms/Search/ItemSearch';
+
+
+const Home: NextPage = () => {
+  return (
+    <>
+    <SearchBar></SearchBar>
+
+    </>
+  )
+}
+
+export default Home
