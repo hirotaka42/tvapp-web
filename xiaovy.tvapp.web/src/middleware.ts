e9d4c37d7cef9f060ca1bf134e066310a639767b
hirotaka42 <kodama.hirotaka.190380@roivy.net>
2024-10-15 21:36:49 +0900

    feat: middlewareを用いたapi呼び出しへの認可処理を仮実装(未完成)

--- xiaovy.tvapp.web/src/middleware.ts
+++ xiaovy.tvapp.web/src/middleware.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server'
 
 // midlewareの使い方は下記を参考
 // https://qiita.com/masakinihirota/items/30a5e06e3288031b9788
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
     // middleware でのconsole.logは、サーバーサイドでのみ出力される
     // フロントには出力されないので注意
     console.log('▶︎Call middleware')
@@ -12,7 +12,25 @@ export function middleware(request: NextRequest) {
     if (request.nextUrl.pathname === '/another') {
         return NextResponse.rewrite(new URL('/rewrite', request.url))
     }
-    return NextResponse.next()
+
+    // 認可処理 (未完成) todo: 認可処理を実装する
+    // Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c
+    const token = await request.headers.get('Authorization')?.split(' ')[1]
+    if (!token) {
+        return NextResponse.json(
+            { message: 'トークンがありません。' },
+            { status: 401 }
+        );
+    }
+    try {
+        console.log('token:', token);
+        return NextResponse.next()
+    }catch {
+        return NextResponse.json(
+            { message: 'トークンが正しくないので、再ログインしてください。' },
+            { status: 401 }
+        );
+    }
 }
 
 export const config = {
