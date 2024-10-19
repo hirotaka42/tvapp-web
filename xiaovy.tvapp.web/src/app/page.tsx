6279ea9269e21ba4fa8e6d22977899dd0f4bde63
hirotaka42 <kodama.hirotaka.190380@roivy.net>
2024-10-19 20:28:21 +0900

    feat: DarkMode切り替えのできるアイコンセレクターを実装

--- xiaovy.tvapp.web/src/app/page.tsx
+++ xiaovy.tvapp.web/src/app/page.tsx
@@ -1,10 +1,11 @@
 import { ThemeSwitch } from "@/app/themeSwitch";
+import { ThemeSelector } from "@/app/themeSelector";
 import { Main } from "@/components/Pages/Main";
 
 export default function Home() {
   return (
     <main className="max-w-5xl mx-auto p-12 lg:p-24">
-      <h2 className="text-2xl font-bold">Light/Dark mode switch on Next.js</h2>
+      <h2 className="text-2xl font-bold">Light/Dark mode switch on Next.js <ThemeSelector /></h2>
       <div className="mt-4 h-10">
         <ThemeSwitch />
       </div>
