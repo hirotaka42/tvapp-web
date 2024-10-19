315367a61ec9768b1cf7319c726f75b2a00d6955
hirotaka42 <kodama.hirotaka.190380@roivy.net>
2024-10-20 01:14:48 +0900

    feat: ItemList表示用のサンプルコンポーネントを作成

--- xiaovy.tvapp.web/src/components/shared/Main.tsx
+++ xiaovy.tvapp.web/src/components/shared/Main.tsx
@@ -1,11 +1,13 @@
 "use client";
 import { FC } from "react";
 import { Home } from "@/components/Pages/Home";
+import Example from "@/components/atomicDesign/molecules/ItemLists"
 
 export const Main: FC = () => {
     return (
         <>
         <h1>Main</h1>
+        <Example />
         <Home />
         </>
     );
