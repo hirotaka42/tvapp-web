701a0ab9aa3bb3bc857a76db106d27b5d8a8cf76
hirotaka42 <kodama.hirotaka.190380@roivy.net>
2024-10-20 21:02:14 +0900

    feat: ジャンルを指定したランキングコンポーネントを表示する際、スマホ表示の時だけ、横表示になるように実装(未完成:画像がどんどん小さくなる)

--- xiaovy.tvapp.web/src/components/atomicDesign/molecules/ContentLists.tsx
+++ xiaovy.tvapp.web/src/components/atomicDesign/molecules/ContentLists.tsx
@@ -7,42 +7,51 @@ interface ExampleProps {
 
 const Example: React.FC<ExampleProps> = ({ contents }) => {
   return (
-    <div 
-      className="min-h-screen dark:bg-black dark:text-white"
-    >
+    <div className="min-h-screen dark:bg-black dark:text-white">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <h2 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">ドラマランキング</h2>
+        <h2 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          ドラマランキング
+        </h2>
 
-        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
+        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-4 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
           {contents.map((content) => (
-            <div key={content.id} className="group relative">
-              <div className="aspect-h-9 aspect-w-16 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75">
+            <a
+              key={content.id}
+              href="#"
+              className="flex flex-row sm:flex-col items-center rounded-lg shadow hover:bg-gray-100 dark:bg-black dark:hover:bg-gray-700 bg-white"
+            >
+              <div className="w-1/3 sm:w-full flex-shrink-0">
                 <Image
                   alt={content.title}
                   src={content.thumbnail.small}
                   width={480}
                   height={270}
-                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                  className="object-cover w-full h-18 rounded-t-lg sm:rounded-none sm:rounded-t-lg"
                 />
               </div>
-              <div className="mt-4 flex justify-between">
-                <div>
-                  <h3 className="text-sm text-gray-700 dark:text-gray-300">
-                    <a href="#">
-                      <span aria-hidden="true" className="absolute inset-0" />
-                      {content.title}
-                    </a>
-                  </h3>
-                  <p className="mt-1 text-sm text-gray-500 dark:text-gray-400">{content.broadcastDateLabel}</p>
-                </div>
-                <p className="text-sm font-medium text-gray-900 dark:text-white">{content.rank}位</p>
+              <div className="p-4 leading-normal w-2/3 sm:w-full">
+                <h5
+                  className="mb-2 text-md font-bold tracking-tight text-gray-900 dark:text-white truncate"
+                  style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}
+                >
+                  {content.title}
+                </h5>
+                <p
+                  className="mb-1 font-normal text-gray-700 dark:text-gray-400 truncate"
+                  style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}
+                >
+                  {content.broadcastDateLabel}
+                </p>
+                <p className="text-sm font-medium text-gray-900 dark:text-white">
+                  {content.rank}位
+                </p>
               </div>
-            </div>
+            </a>
           ))}
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Example;
