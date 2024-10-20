435b6b8ff4071c9380bf8e36ac816c7575038a92
hirotaka42 <kodama.hirotaka.190380@roivy.net>
2024-10-20 15:38:20 +0900

    feat: ホームで取得できるランキングデータ整形のコンバートutile実装

--- xiaovy.tvapp.web/src/components/shared/Main.tsx
+++ xiaovy.tvapp.web/src/components/shared/Main.tsx
@@ -1,17 +1,28 @@
 "use client";
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import { Home } from "@/components/Pages/Home";
 import Example from "@/components/atomicDesign/molecules/ContentLists";
 import { useSessionService } from '@/hooks/useSession';
 import { useTvHomeService } from '@/hooks/useTvHome';
 import { useEpisodeService } from '@/hooks/useEpisode';
 import { useStreamService } from '@/hooks/useStream';
+import { getContentsByLabel, getLabelContentCounts } from '@/utils/Convert/ranking/home/responseParser';
 
 export const Main: FC = () => {
     const session = useSessionService();
     const tvHomeData = useTvHomeService(session);
     const episodInfo = useEpisodeService('epf2lcrt80');
     const streamUrl = useStreamService('epf2lcrt80');
+    useEffect(() => {
+        if (tvHomeData) {
+            const dramaContents = getContentsByLabel(tvHomeData, 'ドラマランキング');
+            const comedyContents = getContentsByLabel(tvHomeData, '笑ってストレス発散！人気コメディドラマ');
+            const contensCount = getLabelContentCounts(tvHomeData);
+            console.log('ドラマ情報', dramaContents);
+            console.log('コンテンツ数', contensCount);
+            console.log('コメディドラマ', comedyContents);
+        }
+    }, [tvHomeData]);
 
     if (!session || !tvHomeData) {
         return <div>Loading...</div>;
