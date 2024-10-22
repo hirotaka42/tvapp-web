5836251b072881f8828e0520a566db38f45e1c63
hirotaka42 <kodama.hirotaka.190380@roivy.net>
2024-10-22 23:33:39 +0900

    feat: ランキングコンテンツをタブで表示できるように実装

--- xiaovy.tvapp.web/src/components/shared/Main.tsx
+++ xiaovy.tvapp.web/src/components/shared/Main.tsx
@@ -2,20 +2,11 @@
 import React, { FC, useEffect, useState } from "react";
 import { useSessionService } from '@/hooks/useSession';
 import { useTvHomeService } from '@/hooks/useTvHome';
-import { useRankingService } from '@/hooks/useRanking';
 import { getContentsByLabel } from '@/utils/Convert/ranking/home/responseParser';
-import { convertEpisodeRankingResponse } from '@/utils/Convert/ranking/genreDetail/responseParser';
 import { convertRankingToCardData } from "@/utils/Convert/ranking/convertRankingToCardData";
 import { ConvertedContent } from '@/types/CardItem/RankingContent';
 import { ContentCardList } from '@/components/atomicDesign/molecules/ContentCardList';
-import { RankingContentCardList } from '@/components/atomicDesign/molecules/RankingContentCardList';
-import { Home } from "@/components/Pages/Home";
-import { TabsWithUnderline } from "@/components/atomicDesign/molecules/Navi/TabsWithUnderLine";
-
-const useGenreRanking = (selectedGenre: string | null) => {
-    const ranking = useRankingService(selectedGenre);
-    return ranking ? convertRankingToCardData(convertEpisodeRankingResponse(ranking)) : null;
-};
+import { TabsWithUnderlineRanking } from "@/components/atomicDesign/molecules/Navi/TabsWithUnderLine-Ranking";
 
 type Tab = {
     title: string;
@@ -26,25 +17,19 @@ export const Main: FC = () => {
     const session = useSessionService();
     const tvHomeData = useTvHomeService(session);
     const [rankingContents, setRankingContents] = useState<Record<string, ConvertedContent[]>>({});
-    const [selectedGenre, setSelectedGenre] = useState<string | null>(null);
-    const [loading, setLoading] = useState<boolean>(false);
-    const sampleTabs: Tab[] = [
+    const sampleGenres: Tab[] = [
         {
           title: "アニメ",
-          query: "Anime",
+          query: "anime",
         },
         {
           title: "ドラマ",
-          query: "Drama",
+          query: "drama",
         },
         {
           title: "バラエティー",
-          query: "Veraety",
-        },
-        {
-          title: "ドキュメンタリー",
-          query: "Documentary",
-        },
+          query: "variety",
+        }
       ];
     
 
@@ -54,9 +39,6 @@ export const Main: FC = () => {
         '↓この放送回、すごく再生されています', '10月開始の新バラエティ番組',
         '【音楽特集】人気番組を見逃し配信中！',
         '今週のイチオシバラエティはこれ！', 'まもなく配信終了'
-    ];
-    const rankingGenres = [
-        'anime', 'variety', 'drama'
     ];
 
     useEffect(() => {
@@ -68,21 +50,6 @@ export const Main: FC = () => {
             setRankingContents(contents);
         }
     }, [tvHomeData]);
-
-    const genreRankingData = useGenreRanking(selectedGenre);
-
-    useEffect(() => {
-        if (selectedGenre) {
-            setLoading(true);
-        }
-    }, [selectedGenre]);
-
-    useEffect(() => {
-        if (genreRankingData && selectedGenre) {
-            setRankingContents(prevContents => ({ ...prevContents, [selectedGenre]: genreRankingData }));
-            setLoading(false);
-        }
-    }, [genreRankingData, selectedGenre]);
 
     if (!session || !tvHomeData) {
         return <div>Loading...</div>;
@@ -98,30 +65,9 @@ export const Main: FC = () => {
                     <ContentCardList contents={rankingContents[label] || []} />
                 </div>
             ))}
-            <TabsWithUnderline
-                tabs={sampleTabs}
+            <TabsWithUnderlineRanking
+                tabs={sampleGenres}
             />
-
-            <div>
-                <h2>ランキングジャンル</h2>
-                {rankingGenres.map(genre => (
-                    <button 
-                        key={genre} 
-                        onClick={() => setSelectedGenre(genre)}
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out"
-                    >
-                        {genre}
-                    </button>
-                ))}
-            </div>
-
-            {selectedGenre && (
-                loading ? 
-                <div>Loading...</div> :
-                <RankingContentCardList contents={rankingContents[selectedGenre] || []} />
-            )}
-
-            <Home />
         </>
     );
 };
