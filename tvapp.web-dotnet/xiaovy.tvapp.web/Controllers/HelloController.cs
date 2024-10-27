88a980ddfc63e4fd9cd98bff587467fcc28d3f0d
hirotaka42 <kodama.hirotaka.190380@roivy.net>
2024-10-27 10:36:01 +0900

    feat: Jsonレスポンスを表示するためのサンプル実装を追加

--- tvapp.web-dotnet/xiaovy.tvapp.web/Controllers/HelloController.cs
+++ tvapp.web-dotnet/xiaovy.tvapp.web/Controllers/HelloController.cs
@@ -1,22 +1,81 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using Newtonsoft.Json;
+using xiaovy.tvapp.web.Models;
 
 namespace xiaovy.tvapp.web.Controllers
 {
     public class HelloController : Controller
     {
-        // GET: /<controller>/
         public IActionResult Index()
         {
-            // 文字列渡し
             ViewData["Message"] = "サンプルメッセージ";
+
+            string jsonData = @"{
+                'contents': [
+                    {
+                        'type': 'episode',
+                        'content': {
+                            'id': 'epjnuys5x5',
+                            'version': 16,
+                            'title': '　',
+                            'seriesID': 'srkq2shp9d',
+                            'endAt': 1729989000,
+                            'broadcastDateLabel': '10月20日(日)放送分',
+                            'isNHKContent': false,
+                            'isSubtitle': false,
+                            'ribbonID': 0,
+                            'seriesTitle': 'ONE PIECE FAN LETTER',
+                            'isAvailable': true,
+                            'broadcasterName': 'フジテレビ',
+                            'productionProviderName': 'フジテレビ'
+                        },
+                        'rank': 1
+                    },
+                    {
+                        'type': 'episode',
+                        'content': {
+                            'id': 'ep7aijfhey',
+                            'version': 6,
+                            'title': '第2話 グロリオ',
+                            'seriesID': 'srg9lxbziz',
+                            'endAt': 1730129340,
+                            'broadcastDateLabel': '10月18日(金)放送分',
+                            'isNHKContent': false,
+                            'isSubtitle': false,
+                            'ribbonID': 0,
+                            'seriesTitle': 'ドラゴンボールDAIMA',
+                            'isAvailable': true,
+                            'broadcasterName': 'フジテレビ',
+                            'productionProviderName': 'フジテレビ'
+                        },
+                        'rank': 2
+                    },
+                    {
+                        'type': 'episode',
+                        'content': {
+                            'id': 'epvvuaba94',
+                            'version': 9,
+                            'title': '#1139「意地悪な弟に困る姉」',
+                            'seriesID': 'srtxft431v',
+                            'endAt': 1729933140,
+                            'broadcastDateLabel': '10月19日(土)放送分',
+                            'isNHKContent': false,
+                            'isSubtitle': true,
+                            'ribbonID': 0,
+                            'seriesTitle': '名探偵コナン',
+                            'isAvailable': true,
+                            'broadcasterName': '読売テレビ',
+                            'productionProviderName': '読売テレビ'
+                        },
+                        'rank': 3
+                    }
+                ]
+            }";
+
+            var contentItems = JsonConvert.DeserializeObject<Dictionary<string, List<ContentModel.ContentItem>>>(jsonData);
+            ViewData["Ranking"] = contentItems["contents"];
             return View();
         }
     }
 }
-
---
+++ tvapp.web-dotnet/xiaovy.tvapp.web/Models/ContentModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace xiaovy.tvapp.web.Models
+{
+	public interface ContentModel
+	{
+        public class Episode
+        {
+            public string Id { get; set; }
+            public int Version { get; set; }
+            public string Title { get; set; }
+            public string SeriesID { get; set; }
+            public long EndAt { get; set; }
+            public string BroadcastDateLabel { get; set; }
+            public bool IsNHKContent { get; set; }
+            public bool IsSubtitle { get; set; }
+            public int RibbonID { get; set; }
+            public string SeriesTitle { get; set; }
+            public bool IsAvailable { get; set; }
+            public string BroadcasterName { get; set; }
+            public string ProductionProviderName { get; set; }
+        }
+
+        public class ContentItem
+        {
+            public string Type { get; set; }
+            public Episode Content { get; set; }
+            public int Rank { get; set; }
+        }
+    }
+}
+
