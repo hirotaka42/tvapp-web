bcc87e87766374e0e1be411c2f05d1c8c42cca12
hirotaka42 <kodama.hirotaka.190380@roivy.net>
2024-11-02 00:12:15 +0900

    feat: headerを常に上部に表示するように実装

--- xiaovy.tvapp.web/src/components/atomicDesign/molecules/Header.tsx
+++ xiaovy.tvapp.web/src/components/atomicDesign/molecules/Header.tsx
@@ -67,8 +67,8 @@ export default function Header() {
 
   return (
     <>
-    <header className="bg-white dark:bg-black">
-      <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+    <header className="bg-white dark:bg-black sticky top-0 z-10">
+      <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between pt-2 pb-2 pl-6 pr-6 lg:px-8">
         <div className="flex lg:flex-1">
           <a href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
@@ -155,8 +155,8 @@ export default function Header() {
         </div>
       </nav>
       <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
-        <div className="fixed inset-0 z-10" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white dark:bg-gray-800 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <div className="fixed inset-0 z-50" />
+        <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white dark:bg-gray-800 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
